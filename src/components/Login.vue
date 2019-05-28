c72ce671af13604f8b8588c046c0c6776347b0e1
Anderson Silva <anderltda@gmail.com>
2019-05-28 16:35:17 -0300

    second commit

---
+++ src/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
--- src/main.js
+++ src/main.js
@@ -13,3 +13,7 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
