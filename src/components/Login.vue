0f8023f83690778758b72900f9c6c89f25de9432
Anderson Silva <anderltda@gmail.com>
2019-05-28 16:35:27 -0300

    second commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,5 +9,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+<style scoped>
+
+</style>
